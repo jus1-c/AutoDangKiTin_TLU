45efac07f7e647272964ee9db24bcf7949bc89dd
Sussy_3k <congthcstp@gmail.com>
2026-01-03 20:31:16 +0700

    Refactor all sources

---
+++ src/config.py
@@ -0,0 +1,34 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    # TLU URLs
+    TLU_LOGIN_URL = os.getenv("TLU_LOGIN_URL")
+    TLU_INFO_URL = os.getenv("TLU_INFO_URL")
+    TLU_SEMESTER_URL = os.getenv("TLU_SEMESTER_URL")
+    TLU_API_BASE_URL = os.getenv("TLU_API_BASE_URL")
+
+    # Google Credentials
+    GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
+    GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
+    
+    # App Settings
+    CONCURRENCY_LIMIT = int(os.getenv("CONCURRENCY_LIMIT", 20))
+    REQUEST_TIMEOUT = int(os.getenv("REQUEST_TIMEOUT", 30))
+    DEBUG = os.getenv("DEBUG", "False").lower() in ("true", "1", "yes")
+    
+    # Paths
+    RES_DIR = "res"
+    TOKEN_FILE = os.path.join(RES_DIR, "token.json")
+    USER_INFO_FILE = os.path.join(RES_DIR, "user_info.json")
+    LOGIN_FILE = os.path.join(RES_DIR, "login.json")
+    GOOGLE_TOKEN_FILE = os.path.join(RES_DIR, "token_google.json")
+
+    @classmethod
+    def ensure_dirs(cls):
+        if not os.path.exists(cls.RES_DIR):
+            os.makedirs(cls.RES_DIR)
+        if not os.path.exists(os.path.join(cls.RES_DIR, "custom")):
+            os.makedirs(os.path.join(cls.RES_DIR, "custom"))
